76b7c09fc079930baaa998a958bff35c224ca6b4
AsankaRukshanRathnayake <rathnay-se19053@stu.kln.ac.lk>
2023-11-16 20:13:16 +0530

    Added function for viewing and closing two sections in home page

---
+++ src/main/webapp/js/script.js
@@ -0,0 +1,10 @@
+function openform(){
+	document.getElementById("reservationForm").style.display = "block";
+	document.getElementById("reservationDetails").style.display = "none";
+}
+
+function openform2(){
+	document.getElementById("reservationDetails").style.display = "block";
+	document.getElementById("reservationForm").style.display = "none";
+	
+}
